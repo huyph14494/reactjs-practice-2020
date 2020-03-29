53d3487ae32ca421751d97da3ac695682dfd75b7
Huy <huy.phamhong@haravan.com>
2020-03-29 13:22:33 +0700

    Updates

---
+++ precache-manifest.3dfe3a2852826939ea1f491a7a0f81d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f6bd0b5c6a9b1bac8c94396db87642",
+    "url": "/reactjs_redux_example/index.html"
+  },
+  {
+    "revision": "6d8e22e35b9cc92253b1",
+    "url": "/reactjs_redux_example/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e17c4968b71ddf6da838",
+    "url": "/reactjs_redux_example/static/css/main.6d27624b.chunk.css"
+  },
+  {
+    "revision": "6d8e22e35b9cc92253b1",
+    "url": "/reactjs_redux_example/static/js/2.2e6b965a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactjs_redux_example/static/js/2.2e6b965a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e17c4968b71ddf6da838",
+    "url": "/reactjs_redux_example/static/js/main.5680d403.chunk.js"
+  },
+  {
+    "revision": "53b82c9dc89557b0b72c",
+    "url": "/reactjs_redux_example/static/js/runtime-main.4ffe7a55.js"
+  }
+]);
