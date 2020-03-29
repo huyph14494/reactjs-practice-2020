658d8057d8dfe5a60ea48c489daf326180cde93f
Huy <huy.phamhong@haravan.com>
2020-03-29 15:49:13 +0700

    fix css moblie

--- src/components/Item.js
+++ src/components/Item.js
@@ -11,37 +11,39 @@ function Item(props) {
 				{/* <th scope="row" className="text-center vertical-align-middle">
 					{props.id + 1}
 				</th> */}
-				<td>
-					<div className="row m-r-l-none">
-						<div className="col-md-8 col-xs-12 text-center vertical-align-middle">
-							<div
-								style={{ maxHeight: 270, maxWidth: 480, height: '45vw', width: '80vw' }}
-								className="text-center frame"
-							>
-								<picture>
-									<img
-										style={{ height: '100%' }}
-										src={PUBLIC_URL + '/images/' + props.data.image}
-										alt={props.data.image}
-										className="img-thumbnail img-fluid"
-									/>
-								</picture>
+				<td className="p-r-l-none">
+					<div className="container">
+						<div className="row">
+							<div className="col-lg-8 col-xs-12 text-center vertical-align-middle">
+								<div
+									style={{ maxHeight: 270, maxWidth: 480, height: '45vw', width: '80vw' }}
+									className="text-center frame"
+								>
+									<picture>
+										<img
+											style={{ height: '100%' }}
+											src={PUBLIC_URL + '/images/' + props.data.image}
+											alt={props.data.image}
+											className="img-thumbnail img-fluid"
+										/>
+									</picture>
+								</div>
 							</div>
-						</div>
-						<div className="col-md-4 col-xs-12 text-left">
-							<p className="font-100-per mt-4">
-								<b>{props.data.name}</b>
-							</p>
-							<p className="font-80-per">{props.data.descriptions}</p>
-							<p className="font-80-per">{props.data.dateNow}</p>
-							<p>
-								<button type="button" className="btn btn-danger mr-3 font-80-per">
-									Danger
-								</button>
-								<button type="button" className="btn btn-warning font-80-per">
-									Warning
-								</button>
-							</p>
+							<div className="col-lg-4 col-xs-12 text-left p-r-l-10-vw">
+								<p className="font-100-per mt-4">
+									<b>{props.data.name}</b>
+								</p>
+								<p className="font-80-per">{props.data.descriptions}</p>
+								<p className="font-80-per">{props.data.dateNow}</p>
+								<p>
+									<button type="button" className="btn btn-danger mr-3 font-80-per">
+										Danger
+									</button>
+									<button type="button" className="btn btn-warning font-80-per">
+										Warning
+									</button>
+								</p>
+							</div>
 						</div>
 					</div>
 				</td>
