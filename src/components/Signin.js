27b0d497c7a9742adcb021e5f86614c793762cd2
Huy <huy.phamhong@haravan.com>
2020-04-05 04:01:17 +0700

    update login

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -3,46 +3,42 @@ const PUBLIC_URL = process.env.PUBLIC_URL;
 
 function Signin(props) {
 	return (
-		<div className="container max-width-420 mt-3">
-			<form className="form-signin">
-				<div className="text-center mb-4">
-					<img
-						className="mb-4"
-						src={PUBLIC_URL + '/images/logo.png'}
-						alt=""
-						width={72}
-						height={72}
-					/>
-					<h1 className="h3 mb-3 font-weight-normal">Floating labels</h1>
-				</div>
-				<div className="form-label-group mb-3">
-					<input
-						type="email"
-						id="inputEmail"
-						className="form-control"
-						placeholder="Email address"
-						required
-					/>
-				</div>
-				<div className="form-label-group mb-3">
-					<input
-						type="password"
-						id="inputPassword"
-						className="form-control"
-						placeholder="Password"
-						required
-					/>
-				</div>
-				<div className="checkbox mb-3 text-left">
-					<label>
-						<input type="checkbox" defaultValue="remember-me" /> Remember me
-					</label>
-				</div>
-				<button className="btn btn-lg btn-primary btn-block" type="submit">
-					Sign in
-				</button>
-				<p className="mt-5 mb-3 text-muted text-center">© 2017-2019</p>
-			</form>
+		<div className="background-login">
+			<div className="container max-width-450 padding-30">
+				<form className="form-signin border-radius-10 background-white padding-30">
+					<div className="text-center mb-4">
+						<img className="mb-4" src={PUBLIC_URL + '/images/logo.png'} alt="" width={72} height={72} />
+						<h1 className="h3 mb-3 font-weight-normal">Floating labels</h1>
+					</div>
+					<div className="form-label-group mb-3">
+						<input
+							type="email"
+							id="inputEmail"
+							className="form-control"
+							placeholder="Email address"
+							required
+						/>
+					</div>
+					<div className="form-label-group mb-3">
+						<input
+							type="password"
+							id="inputPassword"
+							className="form-control"
+							placeholder="Password"
+							required
+						/>
+					</div>
+					<div className="checkbox mb-3 text-left">
+						<label>
+							<input type="checkbox" defaultValue="remember-me" /> Remember me
+						</label>
+					</div>
+					<button className="btn btn-lg btn-primary btn-block" type="submit">
+						Sign in
+					</button>
+					<p className="mt-5 mb-3 text-muted text-center">© 2017-2020</p>
+				</form>
+			</div>
 		</div>
 	);
 }
