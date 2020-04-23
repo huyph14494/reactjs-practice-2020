36970adfba0960eefd6e66e6316a4c9f821540d7
Huy <huy.phamhong@haravan.com>
2020-04-23 18:05:00 +0700

    Updates

---
+++ precache-manifest.2bfb3593665cac1f7f360aba6e51f7fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b36501d17f0e772866870675f932e3",
+    "url": "/reactjs-practice-2020/index.html"
+  },
+  {
+    "revision": "081e8224392591043568",
+    "url": "/reactjs-practice-2020/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "504d88d4dc9f47499527",
+    "url": "/reactjs-practice-2020/static/css/main.d3d6d25d.chunk.css"
+  },
+  {
+    "revision": "081e8224392591043568",
+    "url": "/reactjs-practice-2020/static/js/2.32c08017.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactjs-practice-2020/static/js/2.32c08017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "504d88d4dc9f47499527",
+    "url": "/reactjs-practice-2020/static/js/main.be4c4bdb.chunk.js"
+  },
+  {
+    "revision": "2612008d4df4c6836827",
+    "url": "/reactjs-practice-2020/static/js/runtime-main.151a795c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactjs-practice-2020/precache-manifest.7b81cb2072992e2e1cc9cc6c7fa3adae.js"
+  "/reactjs-practice-2020/precache-manifest.2bfb3593665cac1f7f360aba6e51f7fb.js"
 );
 
 self.addEventListener('message', (event) => {
