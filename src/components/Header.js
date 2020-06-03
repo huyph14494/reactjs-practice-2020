e46be7673d26443b8ee833c173e2c578adf2fd6b
Huy <huy.phamhong@haravan.com>
2020-06-04 01:02:33 +0700

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import 'src/css/Logo.scss';
 
 function Header(props) {
-  console.log("Header");
+	console.log('Header');
 
 	return (
-		<div className="jumbotron m-b-1rem">
-			<h1 className="display-3">Jumbo heading</h1>
-			<p className="lead">Jumbo helper text</p>
+		<div className="jumbotron m-b-1rem jumbotron-padding-custom">
+			<div className="container-logo-parent">
+				<div className="container-logo-son">
+					<section className="logo-react">
+						<div />
+						<div />
+						<div />
+						<div />
+					</section>
+				</div>
+			</div>
+
+			<p className="lead display-4 mt-4">Jumbo heading</p>
 		</div>
 	);
 }
