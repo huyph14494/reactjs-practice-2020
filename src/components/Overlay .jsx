bdff392bfe907399826346d036e49e0234d92e28
Huy <huy.phamhong@haravan.com>
2020-06-05 03:25:24 +0700

    update overlay

--- src/components/Overlay .jsx
+++ src/components/Overlay .jsx
@@ -6,20 +6,24 @@ import 'src/css/Overlay.scss';
 export default function Overlay() {
 	const [ status, setStatus ] = useState(false);
 	const onToggleOverlay = () => {
-		setStatus(!status);
+    setStatus(true);
+    setTimeout(function() {
+      setStatus(false);
+    }, 4500);
 	};
 
 	return (
 		<div>
-			<Button variant="outline-danger" onClick={onToggleOverlay}>
+			<Button variant="outline-danger" className="mt-2 mb-3" onClick={onToggleOverlay}>
 				Overlay
 			</Button>
-			{status ? (
-				<div className='my-overlay' onClick={onToggleOverlay}>
+      {status ? (
+				<div className='my-overlay'>
 					<div className="container-div">
 						<div className="div1">1</div>
 						<div className="div2">2</div>
 						<div className="div3">3</div>
+            <div className="div4">GO!!!</div>
 					</div>
 				</div>
 			) : (
