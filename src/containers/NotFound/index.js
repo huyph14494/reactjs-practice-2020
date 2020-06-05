a04354c3d95085370f7cb44e203c4a4d2c339974
Huy <huy.phamhong@haravan.com>
2020-06-05 20:13:24 +0700

    update

--- src/containers/NotFound/index.js
+++ src/containers/NotFound/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import 'src/containers/NotFound/meteor-shower.scss';
 
 export default function NotFound() {
-  return (
-    <div>
-      Page Not Found!
-    </div>
-  )
+	return (
+		<div className="container-meteor">
+			<div className="space">
+				<div className="planet">
+					<div className="planet_shadow" />
+					<div className="crater1" />
+					<div className="crater2" />
+					<div className="crater3" />
+					<div className="crater4" />
+				</div>
+				<div className="stars">
+					<div className="star" />
+					<div className="star pink" />
+					<div className="star blue" />
+					<div className="star yellow" />
+				</div>
+			</div>
+		</div>
+	);
 }
